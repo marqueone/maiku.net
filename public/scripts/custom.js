34927badef884ebf6de1a0fd125b7ad0ea16c813
Mike <mike.sears@marqueone.net>
2017-04-15 20:16:49 -0400

    more work onthe backend

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -83,37 +83,6 @@
     $(document).ready(function () {
         // Load scripts for each section (portfolio, counter ...)
         sectionsScripts();
-
-        /** Ajax page load settings **/
-        /*(function () {
-
-            if (ajaxLoadingEnabled()) {
-                $(document).pjax('a', '.content-wrap', {fragment: '.content-wrap', scrollTo: false});
-
-                $(document).on('pjax:click', function (event) {
-                    if (!ajaxLoadingEnabled()) {
-                        event.preventDefault();
-                        window.location.reload();
-                    }
-                });
-
-                $(document).on('pjax:end', function () {
-                    document.activeElement && document.activeElement.blur();
-                    sectionsScripts();
-                    // Sections Animation
-                    if (scrollRevealEnabled()) {
-                        scrollReveal(scrollRevealItems.content);
-                    }
-                });
-
-                $(document).on('pjax:beforeReplace', function () {
-                    $('.content-wrap')
-                        .velocity({ opacity: 0 }, { duration: 0 })
-                        .velocity({ opacity: 1 }, { duration: 300, easing: [ 0, 1, 1, 0 ] });
-                });
-            }
-
-        })();*/
     });
 
     /** Set of sections scripts **/
@@ -412,13 +381,13 @@
 })(jQuery);
 
 /** Google Analytics **/
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-96835614-1', 'auto');
-  ga('send', 'pageview');
+//   ga('create', 'UA-96835614-1', 'auto');
+//   ga('send', 'pageview');
 
 
 
